888fda86a10649d8dfa58ac0091819b9050008df
Renan Muniz <fmrenan@outlook.com>
2021-05-03 14:33:18 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1 className="text-primary">Hello guys</h1>
+      <DataTable />
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Footer from 'components/Footer';
+import Header from 'components/Header';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
