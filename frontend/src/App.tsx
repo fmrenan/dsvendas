ba47b17864fd521e5babfc40822b6162acb9b6eb
Renan Muniz <fmrenan@outlook.com>
2021-05-03 12:02:44 -0300

    Bootstraped!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello guys</h1>
+      <h1 className="text-primary">Hello guys</h1>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
