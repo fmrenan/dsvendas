7466e6120de00fedd295a6d3a01a977cbbf4ba32
Renan Muniz <fmrenan@outlook.com>
2021-05-07 18:55:40 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,11 @@
+import Pagination from "components/Pagination"
 import { useEffect, useState } from "react"
 import { api } from "services/api"
 import { SalePage } from "types/sale"
 import { formatLocalDate } from "utils/format"
 
-export default function DataTable(){
-  const[page, setPage] = useState<SalePage>({
+export default function DataTable() {
+  const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
     number: 0,
@@ -13,39 +14,46 @@ export default function DataTable(){
   })
   const [currentPage, setCurrentPage] = useState<number>(0)
 
-  useEffect( () => {
+  useEffect(() => {
     api.get(`sales?page=${currentPage}&size=20&sort=date,desc`)
       .then((res) => {
         setPage(res.data)
       })
-  },[])
+  }, [currentPage])
 
-  return(
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map(item => (
-            <tr key={item.id}>
-              <td>{formatLocalDate(item.date, 'dd/MM/YYY')}</td>
-              <td>{item.seller.name}</td>
-              <td>{item.visited}</td>
-              <td>{item.deals}</td>
-              <td>{item.amount.toFixed(2)}</td>
+  function handleOnPageChange(index: number){
+    setCurrentPage(index)
+  }
+
+  return (
+    <>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
             </tr>
-          ))}
-          
-          
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {page.content?.map(item => (
+              <tr key={item.id}>
+                <td>{formatLocalDate(item.date, 'dd/MM/YYY')}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+              </tr>
+            ))}
+
+
+          </tbody>
+        </table>
+      </div>
+      <Pagination page={page} onPageChange={handleOnPageChange}/>
+    </>
   )
 }
