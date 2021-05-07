94cabee99b2a68b7764968b1b72e6aaabfd760cf
Renan Muniz <fmrenan@outlook.com>
2021-05-07 18:32:50 -0300

    DataTable Integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react"
+import { api } from "services/api"
+import { SalePage } from "types/sale"
+import { formatLocalDate } from "utils/format"
+
 export default function DataTable(){
+  const[page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0
+  })
+  const [currentPage, setCurrentPage] = useState<number>(0)
+
+  useEffect( () => {
+    api.get(`sales?page=${currentPage}&size=20&sort=date,desc`)
+      .then((res) => {
+        setPage(res.data)
+      })
+  },[])
+
   return(
     <div className="table-responsive">
       <table className="table table-striped table-sm">
@@ -12,34 +33,17 @@ export default function DataTable(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
+          {page.content?.map(item => (
+            <tr key={item.id}>
+              <td>{formatLocalDate(item.date, 'dd/MM/YYY')}</td>
+              <td>{item.seller.name}</td>
+              <td>{item.visited}</td>
+              <td>{item.deals}</td>
+              <td>{item.amount.toFixed(2)}</td>
+            </tr>
+          ))}
+          
+          
         </tbody>
       </table>
     </div>
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller";
+
+export interface Sale{
+  id: number
+  visited: number
+  deals: number
+  amount: number
+  date: string
+  seller: Seller
+}
+
+export interface SalePage{
+  content?: Sale[]
+  last: boolean
+  totalPages: number
+  totalElements: number
+  number: number
+  first: boolean
+  size?: number
+  numberOfElements?: number
+  empty?: boolean
+}
+
 export interface SaleSum {
   sellerName: string
   sum: number
